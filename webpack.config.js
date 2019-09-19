9d518afbd1a031d8033ed78f3c4a8c55303601ab
Default Profile <test@gmail.com>
2019-09-19 11:03:12 -0600

    Added default pollyfill

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: ['./src/js/index.js'],
+    entry: ['@babel/polyfill', './src/js/index.js'],
     // mode: "development",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js',
-        publicPath: '/dist/'
+        publicPath: './'
     },
     devServer: {
         historyApiFallback: true,
