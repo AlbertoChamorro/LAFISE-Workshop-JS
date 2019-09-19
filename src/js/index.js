ebf0b928ecb88f280abea80e807bde139923d240
Default Profile <test@gmail.com>
2019-09-19 10:05:06 -0600

    Configure loaders webpack

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,15 @@
+import '../styles/main.css'
 import artirstService from './api/artists.services'
 
 console.log("Loading...")
 
 artirstService.getTracks("Believe")
     .then(response => {
-        console.log(response)
+        const res = response
+        console.log(res)
     }).catch(error => {
         console.log(error)
     })
+
+let newObject = Object.assign({}, {a: 1, b: 3})
+console.log(newObject)
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,54 @@
 const path = require('path')
 const webpack = require ("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: ['./src/js/index.js'],
-    mode: "development",
+    // mode: "development",
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js',
         publicPath: '/dist/'
     },
     devServer: {
+        historyApiFallback: true,
         contentBase: path.join(__dirname, 'dist'),
+        writeToDisk: true,
         compress: false,
         port: 9000,
-        writeToDisk: true,
         publicPath: '/dist/'
-      }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: { loader: 'babel-loader' }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 };
